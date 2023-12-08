6c03f67193b0fa85120acce02d1b6c9777c6c6cc
CedarTheNoble <112874841+KacperCedro@users.noreply.github.com>
2023-12-08 11:19:37 +0100

    new changes

--- 24.11.2023/zadanie5.php
+++ 24.11.2023/zadanie5.php
@@ -4,6 +4,8 @@
     $Eo = 0.0000001;
     $Ex = 0.0000001;
 
+    
+
     function GetX0($a, $b, $Eo, $Ex) {
         $Fa = FunctionToCalcualte($a);
         $Fb = FunctionToCalcualte($b);
@@ -17,12 +19,21 @@
             $aMinusB = (($a - $b) > 0) ? (($a - $b) * 1) : (($a - $b) * (-1));
             while($aMinusB > $Ex){
                 $Xo = Xby0($a, $b);
-                $FoAbsolute = (FunctionToCalcualte($Xo) > 0) ? (FunctionToCalcualte($Xo) * 1) : (FunctionToCalcualte($Xo) * (-1));
-                if ($Fo ) {
-                    # code...
+                $Fo = FunctionToCalcualte($Fo);
+                $FoAbsolute = ($Fo > 0) ? ($Fo * 1) : ($Fo * (-1));
+                if ($FoAbsolute < $Eo ) {
+                    break;
+                }
+                elseif(($Fa * $Fo) < 0){
+                    $b = $Xo;
+                }
+                else {
+                    $a = $Xo;
+                    $Fa = $Fo;
                 }
             }
         }
+        return $Xo;
     }
     function Xby0($a, $b) {
         return (($a + $b)/ 2);
